1ed8456d1825178417dee4d8a451085ffe2037a3
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2023-11-21 12:36:52 +0530

    Update ordersRouter.js

--- Router/OrdersRouter/ordersRouter.js
+++ Router/OrdersRouter/ordersRouter.js
@@ -50,59 +50,7 @@ router.post("/", async (req, res) => {
     });
 
     
-    router.get("/admin", async (req, res) => {
-      const {query}=req.query
-        
-        try { 
-          let order
-          if(query){
-            order = await Order.find(query)
-          }
-          else{
-            order = await Order.find()
-          }
-          
-          if (!order) {
-            res.status(400).json({ message: "can't get the Orders data" });
-          }
-          res.status(200).json(order);
-        } catch (error) {
-          console.log(error);
-          res.status(500).json("Server Error");
-        }
-      });
-  
-
-
-      router.delete("/admin/:id",async(req,res)=>{
-        try {
-            const deleteOrder=await Order.findByIdAndDelete(
-                {_id:req.params.id},
-            )   
-            if(!deleteOrder){return res.status(400).json({message:"Couldn'nt delete your content"})}
-            return res.status(200).json({message:"Deleted Successfully"})
-        } catch (error) {
-            console.log(error);
-            res.status(500).json({message:"Internal server error"})
-        }
-    
-        
-    })
-
-    router.delete("/",async(req,res)=>{
-      try {
-          const deleteWholeOrder=await Order.deleteMany(
-              {},
-          )   
-          if(!deleteWholeOrder){return res.status(400).json({message:"Couldn'nt delete your content"})}
-          return res.status(200).json({message:"Deleted Successfully"})
-      } catch (error) {
-          console.log(error);
-          res.status(500).json({message:"Internal server error"})
-      }
-  
-      
-  })
+ 
 
 
 export const orderRouter=router
