0c7178547f4d107223f2b700abae931ed54c47e6
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2023-11-23 15:53:47 +0530

    Update productRouter.js

--- Router/Products/productRouter.js
+++ Router/Products/productRouter.js
@@ -5,18 +5,36 @@ const router=express.Router();
 
 
 router.get("/", async (req, res) => {
+  const {product}=req.query;
+  console.log(product)
     try {
+      if(product){
+        const products = await Products.find({$or:[
+        {name:{$regex:product,$options:`i`}},
+        {varient:{$regex:product,$options:`i`}},
+        {type:{$regex:product,$options:`i`}},
+        {brand:{$regex:product,$options:`i`}},
+        {productGroup:{$regex:product,$options:`i`}},
+        {productSubGroup:{$regex:product,$options:`i`}}]});
+        if (!products) {
+          res.status(400).json({ message: "can't get the data" });
+        }
+        res.status(200).json(products);
+      }
+      else{
       const products = await Products.find({});
       if (!products) {
         res.status(400).json({ message: "can't get the data" });
       }
       res.status(200).json(products);
+    }
     } catch (error) {
       console.log(error);
       res.status(500).json("Server Error");
     }
   });
 
+ 
 
 
 
